6200890e8e77db673130dd9b3d00acb7c42ad30c
Jacobo Olmedo Sánchez <jacobo.olmedo.sanchez@udc.es>
2025-09-26 17:46:24 +0200

    req + empezar

--- Q_learning.py
+++ Q_learning.py
@@ -1,6 +1,43 @@
 from robobopy.Robobo import Robobo
 import gymnasium as gym
+#from envRobobo import envRobobo
+from robobosim.RoboboSim import RoboboSim
+from robobopy.utils.IR import IR
+
+
+# robobo = Robobo('localhost')
+# robobo.connect()
+# sim = RoboboSim("localhost")
+# sim.connect()
+# env = envRobobo()
+# obs, info = env.reset()
+# total_reward = 0
+# done = False
+# while not done:
+#     action = env.action_space.sample()
+#     obs = (robobo.readAllIRSensor())
+#     print(obs)
+#     # obs, reward, terminated, truncated, info = env.step(action)
+#     # total_reward += reward
+#     # done = terminated or truncated
+# print(f"episodio acabado, recompensa total: {total_reward}")
+# env.close()
+
+from robobopy.Robobo import Robobo
+import time
 
 robobo = Robobo('localhost')
 robobo.connect()
+
+
+def main():
+    robobo.moveWheelsByTime(30, 30, 0.5)
+    irs = robobo.readAllIRSensor()
+    if irs:
+        for sensor_name, sensor_value in irs.items():
+            print(f"Sensor {sensor_name}: {sensor_value}")
+    else:
+        print("No hay datos de sensores IR disponibles")
+main()
+
 
--- envRobobo.py
+++ envRobobo.py
@@ -1,24 +1,44 @@
-import gymnasium as gym
-from robobopy.Robobo import Robobo
-from robobosim.RoboboSim import RoboboSim
+# import gymnasium as gym
+# from robobopy.Robobo import Robobo
+# from robobosim.RoboboSim import RoboboSim
 
-sim = RoboboSim("localhost")
-sim.connect()
+# class envRobobo(gym.Env):
+#     def __init__(self):
+#         super().__init__()
+#         self.robobo = Robobo('localhost')
+#         self.robobo.connect()
+#         self.sim = RoboboSim("localhost")
+#         self.sim.connect()
+#         self.action_space = gym.spaces.Discrete(4)  # 4 acciones posibles
+#         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(8,), dtype=int)  # 8 IR sensores
 
-class envRobobo(gym.Env):
-    def __init__(self):
-        super().__init__()
-        self.robobo = Robobo('localhost')
-        self.robobo.connect()
-        self.action_space = gym.spaces.Discrete(4)  # 4 acciones posibles
-        self.observation_space = gym.spaces.Discrete(6)  # 6 estados posibles (podemos empezar con menos)
+#     def step(self, action):
+#         # Ejecuta la acción en el robot
+#         if action == 0:
+#             self.robobo.moveWheels(25, 25)
+#         elif action == 1:
+#             self.robobo.moveWheels(-25, -25)
+#         elif action == 2:
+#             self.robobo.moveWheels(-5, 15)
+#         elif action == 3:
+#             self.robobo.moveWheels(15, -5)
+#         self.sim.wait(1)
 
-    def step(self, action):
-        # logica para ejecutar una acción y obtener el nuevo estado, recompensa...
-        pass
+#         # Observa estado
+#         obs = (self.robobo.readAllIRSensor())
+#         print(obs)
+#         # Recompensa positiva si ningún sensor detecta obstáculo cerca
+#         # reward = 1 if min(obs) > 100 else -10
+#         # terminated = min(obs) <= 20  # Termina episodio si muy cerca de obstáculo
+#         # return obs, reward, terminated, False, {"info": "estado"}
 
-    def reset(self):
-        sim.resetSimulation()
 
-    def close(self):
-        self.robobo.disconnect()
+#     def reset(self, seed=None, options=None):
+#         self.sim.resetSimulation()
+#         # Supón que obtienes la observación con robobo.readAllIRSensor()
+#         observation = self.robobo.readAllIRSensor()
+#         return observation, {}
+
+
+#     def close(self):
+#         self.robobo.disconnect()
